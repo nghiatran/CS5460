578cef58b44db44c5f4479d0510f2e0222ea2059
nghiatran <nghiatdinh@gmail.com>
2016-03-15 21:51:06 -0400

    use 2 range bound fields in work structure

---
+++ lab2/p_factorizer.c
@@ -0,0 +1,156 @@
+#include <stdio.h>
+#include "mw_api.h"
+#include "math.h"
+#include <gmp.h>
+#include <string.h>
+#include "factorizer.h"
+
+#define MAX_DIGITS 40
+#define MAX_FACTORS_PER_WORK 1000
+
+struct userdef_work_t{
+  char val[MAX_DIGITS];
+  char lower_bound[MAX_DIGITS];
+  char upper_bound[MAX_DIGITS];
+};
+
+
+struct userdef_result_t{
+  char factors[2*MAX_FACTORS_PER_WORK][MAX_DIGITS];
+  //char factor[MAX_DIGITS];
+};
+
+mw_result_t *do_work(mw_work_t *work){
+  mw_result_t *result = malloc(sizeof(mw_result_t));
+  mpz_t val;
+  mpz_init(val);
+  mpz_set_str(val,work->val,10);
+
+  mpz_t lower_bound;
+  mpz_init(lower_bound);
+  mpz_set_str(lower_bound,work->lower_bound,10);
+
+  mpz_t upper_bound;
+  mpz_init(upper_bound);
+  mpz_set_str(upper_bound,work->upper_bound,10);
+  //printf("doing work\n");
+  struct factor_node * factor_nodes = find_factors(lower_bound,upper_bound,val);
+
+  int i =0;
+  struct factor_node * iter = factor_nodes;
+  while(iter != NULL){
+    strcpy(result->factors[i++],mpz_get_str(NULL,10,iter->factor));
+    iter = iter->next;
+  }
+
+  for(;i<2*MAX_FACTORS_PER_WORK;i++)
+    result->factors[i][0] = '\0';
+
+  // for(int i =0;i<MAX_FACTORS_PER_WORK;i++){
+  //   if(strlen(work->factors[i]) != 0){
+  //     mpz_t factor;
+  //     mpz_init(factor);
+  //     mpz_set_str(factor,work->factors[i],10);
+  //     if(mpz_divisible_p(val,factor) != 0){
+  //       strcpy(result->factors[2*i],work->factors[i]);
+  //       mpz_t q;
+  //       mpz_init(q);
+  //       mpz_cdiv_q(q,val,factor);
+  //       strcpy(result->factors[2*i+1],mpz_get_str(NULL,10,q));  
+  //     }
+  //     else{
+  //       result->factors[2*i][0] = '\0';
+  //       result->factors[2*i+1][0] = '\0';
+  //     }
+  //   }
+  //   else{
+  //       result->factors[2*i][0] = '\0';
+  //       result->factors[2*i+1][0] = '\0';
+  //   }
+  // }
+
+  return result;
+}
+
+
+mw_work_t **create_work(int argc,char **argv){
+
+  char val[MAX_DIGITS];
+  strncpy(val,argv[1],MAX_DIGITS);
+  mpz_t mp_val;
+  mpz_init(mp_val);
+  mpz_set_str(mp_val,val,10);
+
+  mpz_t mp_sqrtn;
+  mpz_init(mp_sqrtn);  
+  mpz_sqrt (mp_sqrtn, mp_val);
+  unsigned long int sqrtn = mpz_get_ui(mp_sqrtn);
+
+  unsigned long int nWorks = (sqrtn + MAX_FACTORS_PER_WORK -1)/MAX_FACTORS_PER_WORK;
+  int remain = sqrtn % MAX_FACTORS_PER_WORK;
+  
+  mw_work_t ** works = malloc(sizeof(mw_work_t*) * (nWorks+1));
+
+  mpz_t f;
+  mpz_init(f);
+  mpz_set_str(f,"2",10);
+  for(int i=0;i<nWorks;i++){
+    mw_work_t *wrk = malloc(sizeof(mw_work_t));
+    strcpy(wrk->val,val);
+    int count = i == nWorks -1 && remain != 0 ? remain : MAX_FACTORS_PER_WORK;
+
+    char *lower_bound = mpz_get_str(NULL,10,f);
+    mpz_add_ui(f,f,count-1);
+    char *upper_bound = mpz_get_str(NULL,10,f);
+    mpz_add_ui(f,f,1);
+
+    strcpy(wrk->lower_bound,lower_bound);    
+    strcpy(wrk->upper_bound,upper_bound);
+
+    *(works+i) = wrk;
+  }
+  *(works+nWorks) = NULL;
+  return works;
+}
+
+
+int * process_results(int sz, mw_result_t *res){
+  int factors_count=0;
+  for(int i=0;i<sz;i++){
+    for(int j =0;j<2*MAX_FACTORS_PER_WORK;j++){
+      if (strlen((res+i)->factors[j]) != 0)
+        factors_count++;
+    }
+  }
+  char results[factors_count][MAX_DIGITS];
+  int c =0;
+  for(int i=0;i<sz;i++){
+    for(int j =0;j<2*MAX_FACTORS_PER_WORK;j++){
+      if (strlen((res+i)->factors[j]) != 0){
+        strcpy(results[c++],(res+i)->factors[j]);
+      }
+    }
+  }
+  printf("The number has %d factors\n",factors_count);
+  return 1;
+}
+
+
+
+int main(int argc, char **argv)
+{
+  struct mw_api_spec f;
+
+  MPI_Init (&argc, &argv);
+
+  f.create = create_work;
+  f.result = process_results;
+  f.compute = do_work;
+  f.work_sz = sizeof (struct userdef_work_t);
+  f.res_sz = sizeof (struct userdef_result_t);
+  MW_Run (argc, argv, &f);
+  MPI_Finalize ();
+
+  return 0;
+
+}
---
+++ lab2/s_factorizer.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "mw_api.h"
+#include "math.h"
+#include <gmp.h>
+#include <string.h>
+#include "factorizer.h"
+
+int main(int argc, char **argv)
+{
+  mpz_t val;
+  mpz_init(val);
+  mpz_set_str(val,argv[1],10);
+
+  mpz_t mp_sqrt;
+  mpz_init(mp_sqrt);  
+  mpz_sqrt (mp_sqrt, val);
+
+
+  mpz_t i;
+  mpz_init(i);
+  mpz_set_str(i,"2",10);
+
+  struct factor_node * factors = find_factors(i,mp_sqrt,val);
+
+  mpz_t count;
+  mpz_init(count);
+  mpz_set_str(count,"0",10);
+  struct factor_node * iter = factors;
+  while(iter != NULL){
+    //printf("factor is %s\n",mpz_get_str(NULL,10,iter->factor));
+    iter = iter->next;
+    mpz_add_ui(count,count,1);
+  }
+  printf("The number has %s factors\n",mpz_get_str(NULL,10,count));
+
+  return 0;
+}
+
+
